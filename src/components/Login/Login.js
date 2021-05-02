25e858733e59e36cfac32cf90c635f97fdc02bad
Asif Mahmud <asiffyy@gmail.com>
2021-05-02 21:24:47 +0600

    Imported necessary assets and writing login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,7 +2,21 @@ import React from "react";
 import style from "./Login.module.css";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <img />
+        <h1>Quick Chat</h1>
+        <button>Sign in with Google</button>
+      </div>
+      <div>
+        <img />
+      </div>
+      <div>
+        <img />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
