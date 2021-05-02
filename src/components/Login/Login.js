3aaaff2ada7e8dc5f4e17f27bdfe0e6316f5c648
Asif Mahmud <asiffyy@gmail.com>
2021-05-02 21:29:05 +0600

    Markup for login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,22 @@
 import React from "react";
 import style from "./Login.module.css";
+import primaryLogo from "../../assets/primary-logo.svg";
+import topSideArt from "../../assets/top-login-art.svg";
+import bottomSideArt from "../../assets/bottom-login-art.svg";
 
 const Login = () => {
   return (
     <div>
       <div>
-        <img />
+        <img src={primaryLogo} />
         <h1>Quick Chat</h1>
         <button>Sign in with Google</button>
       </div>
       <div>
-        <img />
+        <img src={topSideArt} />
       </div>
       <div>
-        <img />
+        <img src={bottomSideArt} />
       </div>
     </div>
   );
