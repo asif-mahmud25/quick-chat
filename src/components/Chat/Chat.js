cf89a717d28038ff3f870225ead1d4d7b3dd8a4f
Asif Mahmud <asiffyy@gmail.com>
2021-05-03 22:58:06 +0600

    markup for chat component

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,17 @@
+import React from "react";
+import style from "./Chat.module.css";
+
+const Chat = () => {
+  return (
+    <div>
+      <h1>Dev Room</h1>
+      <div></div>
+      <div>
+        <input type="text" />
+        <button>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
