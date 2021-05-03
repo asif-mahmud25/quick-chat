91fc18652aa189a39c6490d554f891fe5710891d
Asif Mahmud <asiffyy@gmail.com>
2021-05-03 15:53:31 +0600

    sidebar styles

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,17 +6,19 @@ import Room from "../Room/Room";
 
 const Sidebar = () => {
   return (
-    <div>
-      <div>
+    <div className={style.sidebar}>
+      <div className={style.logo}>
         <img src={secondaryLogo} alt="" />
         <h1>Quick Chat</h1>
       </div>
-      <div>
-        <img src={userImg} alt="User Image" />
-        <h1>Asif Mahmud</h1>
-        <p>Logout</p>
+      <div className={style.userSection}>
+        <div className={style.userInfo}>
+          <img src={userImg} alt="User Image" />
+          <h1>Asif Mahmud</h1>
+          <p>Logout</p>
+        </div>
       </div>
-      <h1>Add New Room</h1>
+      <h1 className={style.addRoom}>Add New Room</h1>
       <div>
         <Room />
         <Room />
