df538872f16c901050e19bbd6ebdae2a6efded1b
Asif Mahmud <asiffyy@gmail.com>
2021-05-04 01:23:24 +0600

    message style implemented

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Message from "../Message/Message";
 import style from "./Chat.module.css";
 
 const Chat = () => {
   return (
     <div className={style.chat}>
       <h1>Dev Room</h1>
-      <div className={style.chatBox}></div>
+      <div className={style.chatBox}>
+        <Message msgFrom="user" />
+        <Message msgFrom="other" />
+        <Message msgFrom="user" />
+        <Message msgFrom="other" />
+      </div>
       <div className={style.inputBoxContainer}>
         <div className={style.inputBox}>
           <input type="text" placeholder="Enter your message here" />
--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -2,19 +2,33 @@ import React from "react";
 import style from "./Message.module.css";
 import userIcon from "../../assets/room-icon.svg";
 
-const Message = () => {
+const Message = (props) => {
+  let messageContainer = style.messageContainer;
+  let messageTxtWrapper = style.messageTxtWrapper;
+  let senderName = style.senderName;
+  let messageTxt = style.messageTxt;
+  let timeStamp = style.timeStamp;
+
+  if (props.msgFrom === "user") {
+    messageContainer = style.messageContainerUser;
+    messageTxtWrapper = style.messageTxtWrapperUser;
+    senderName = style.senderNameUser;
+    messageTxt = style.messageTxtUser;
+    timeStamp = style.timeStampUser;
+  }
+
   return (
-    <div>
+    <div className={messageContainer}>
       <img src={userIcon} alt="" />
       <div>
-        <p>Sayem Hasan</p>
-        <div>
-          <p>
+        <div className={messageTxtWrapper}>
+          <p className={senderName}>Sayem Hasan</p>
+          <p className={messageTxt}>
             Es un hecho establecido hace demasiado tiempo que un lector se
             distraerá con el contenido
           </p>
+          <p className={timeStamp}>12:30pm, 21 Mar</p>
         </div>
-        <p>12:30pm, 21 Mar</p>
       </div>
     </div>
   );
