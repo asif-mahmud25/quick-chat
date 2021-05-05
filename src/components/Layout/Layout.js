bbbffa505d62395316599b17382d31b1669c2b7a
Asif Mahmud <asiffyy@gmail.com>
2021-05-05 21:53:35 +0600

    implementing firebase authentication

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Chat from "../Chat/Chat";
+import Login from "../Login/Login";
 import Sidebar from "../Sidebar/Sidebar";
 import style from "./Layout.module.css";
 
 const Layout = () => {
   return (
     <div className={style.layout}>
-      <div className={style.sidebarContainer}>
+      {/* <div className={style.sidebarContainer}>
         <Sidebar />
       </div>
       <div className={style.chatContainer}>
         <Chat />
-      </div>
+      </div> */}
+
+      <Login />
     </div>
   );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,14 +3,24 @@ import style from "./Login.module.css";
 import primaryLogo from "../../assets/primary-logo.svg";
 import topSideArt from "../../assets/top-login-art.svg";
 import bottomSideArt from "../../assets/bottom-login-art.svg";
+import { auth, provider } from "../../firebase";
 
 const Login = () => {
+  const login = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((res) => console.log(res.user.displayName))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className={style.login}>
       <div className={style.loginMain}>
         <img src={primaryLogo} alt="" />
         <h1>Quick Chat</h1>
-        <button className={style.ripple}>Sign in with Google</button>
+        <button className={style.ripple} onClick={login}>
+          Sign in with Google
+        </button>
       </div>
       <div className={style.decorationTop}>
         <img src={topSideArt} alt="" />
